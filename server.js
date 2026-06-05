45a51afd5a18d8dd029493845ec04f7b6dfe1253
AinzYriel <rileyzcastillo706@gmail.com>
2026-06-05 11:41:40 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/RetroCalc v0.8.html');
 });
 
 app.listen(3000);
